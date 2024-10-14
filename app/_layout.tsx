fe3b8e5e3826f6465d153035ef68ff922e8e51b4
user-ank <ankush1maji@gmail.com>
2024-10-15 02:17:54 +0530

    Database added to the project
    .db file can be used for database

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,28 +2,80 @@ import 'react-native-gesture-handler';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
 import CustomDrawerLayout from '@/components/CustomDrawerLayout';
+import * as FileSystem from "expo-file-system";
+import { useFonts } from 'expo-font';
+import { useEffect, useState } from 'react';
+import { SplashScreen } from 'expo-router';
+import { Asset } from 'expo-asset';
+import { ActivityIndicator, View } from 'react-native';
+import { SQLiteProvider } from 'expo-sqlite/next';
+
+const loadDatabase = async () => {
+  const dbName = "moneyZDB.db";
+  const dbAsset = require("../assets/moneyZDB.db");
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+  if (!fileInfo.exists) {
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true }
+    );
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+};
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+  
+  const [dbLoaded, setDbLoaded] = useState<boolean>(false);
+  useEffect(() => {
+    loadDatabase()
+    .then(() => setDbLoaded(true))
+    .catch((e) => console.error(e));
+  }, []);
+  
+  if (!loaded) {
+    return null;
+  }
+  if (!dbLoaded)
+    return (
+      <View style={{ flex: 1 , backgroundColor: 'red'}}>
+        <ActivityIndicator size={"large"} />
+      </View>
+  );
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <SQLiteProvider databaseName="moneyZDB.db" useSuspense>
         <Drawer drawerContent={CustomDrawerLayout} 
         screenOptions={{ 
         drawerActiveTintColor: '#004c3f',
-        headerStyle: {backgroundColor : '#fffde8'}
+        headerStyle: {backgroundColor : '#fffde8'},
+        headerShadowVisible: true
         }}>
-        <Drawer.Screen
-            name="index" // This is the name of the page and must match the url from root
-            options={{
-                drawerLabel: 'Home',
-                title: 'MoneyMadness',
-                headerTransparent: true,
-            }}
-        />
+          <Drawer.Screen
+              name="index" // This is the name of the page and must match the url from root
+              options={{
+                  drawerLabel: 'Home',
+                  title: 'MoneyMadness',
+                  headerTransparent: true,
+              }}
+          />
           <Drawer.Screen
                name="personalExpense" // This is the name of the page and must match the url from root
                options={{
                    drawerLabel: 'Personal Expenses',
                    title: 'Personal Expenses',
+                   headerShadowVisible: true
                 }}
             />
             <Drawer.Screen
@@ -47,6 +99,7 @@ export default function RootLayout() {
                 }}
             />
         </Drawer>
+      </SQLiteProvider>
     </GestureHandlerRootView>
   );
 }
